9b6d99016a4eac32cecad314c072dd342e1fdd0c
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2017-11-04 20:35:32 +0100

    added floor

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
 
 function preload() {
     console.log("preload")
@@ -6,14 +6,28 @@ function preload() {
 
 }
 
+var floor, c
+
 function create() {
     console.log("create")
     game.physics.startSystem(Phaser.Physics.ARCADE)
-    var c = game.add.sprite(0,0,'test')
+
+    floor = game.add.group()
+    floor.enableBody = true
+
+    for (var i = 0; i < 10; i++) {
+        var tile = floor.create(i * 32, 300, 'test')
+        tile.body.immovable = true
+    }
+
+
+    c = game.add.sprite(40, 0, 'test')
     game.physics.arcade.enable(c)
     c.body.gravity.y = 100
+
 
 }
 
 function update() {
+    game.physics.arcade.collide(c, floor)
 }

9f9cdf7b0d8e39b77cd33d0f91ad5065570234ef
gantneba <gantneba@gmail.com>
2017-11-06 15:52:08 +0100

    added enemy, death on hit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 var character, floor
 var speed = 4
 var arrow
+var enemy
 
 var fireRate = 100;
 var nextFire = 0;
@@ -17,6 +18,7 @@ function preload() {
     game.load.image('groundTile6', 'assets/kenney_platformerpack_industrial/PNG/Default_size/platformIndustrial_006.png')
     game.load.image('groundTile7', 'assets/kenney_platformerpack_industrial/PNG/Default_size/platformIndustrial_007.png')
     game.load.image('groundTile8', 'assets/kenney_platformerpack_industrial/PNG/Default_size/platformIndustrial_008.png')
+    game.load.image('enemy1', 'assets/kenney_platformerpack_industrial/PNG/Default_size/platformIndustrial_040.png')
     game.load.image('char', 'assets/kenney_platformerpack_industrial/PNG/Default_size/platformIndustrial_055.png')
     game.load.image('arrow2', 'assets/arrow_minecraft_1.png')
     game.load.image('arrow', 'assets/kenney_platformerpack_industrial/PNG/Default_size/platformIndustrial_070.png')
@@ -26,28 +28,32 @@ function create() {
     console.log("create")
     game.physics.startSystem(Phaser.Physics.ARCADE)
 
+
     floor = game.add.group()
     floor.enableBody = true
+    arrow = game.add.group();
+    arrow.enableBody = true;
 
-    for (var i = 0; i < 20; i++) {
+    enemy = game.add.group()
+    enemy.enableBody = true
+
+    for (var i = 0; i < 30; i++) {
         var tile = floor.create(i * 32, 300, 'test')
         tile.body.immovable = true
     }
-
-    arrow = game.add.group();
-    arrow.enableBody = true;
-    arrow.physicsBodyType = Phaser.Physics.ARCADE;
-    arrow.createMultiple(50, 'arrow');
-
-    arrow.setAll('checkWorldBounds', true);
-    arrow.setAll('outOfBoundsKill', true);
-    //c = game.add.sprite(40, 0, 'test')
-
     floor.create(0, 360, 'groundTile1');
     floor.create(0, 400, 'groundTile2');
     floor.create(200, 120, 'groundTile6');
     floor.create(-60, 120, 'groundTile7');
     floor.create(900, 170, 'groundTile8');
+
+    arrow.physicsBodyType = Phaser.Physics.ARCADE;
+    arrow.createMultiple(50, 'arrow');
+    arrow.setAll('checkWorldBounds', true);
+    arrow.setAll('outOfBoundsKill', true);
+    //c = game.add.sprite(40, 0, 'test')
+
+
 
     character = game.add.sprite(320, 240, 'char');
     game.physics.arcade.enable(character)
@@ -55,16 +61,29 @@ function create() {
     character.body.collideWorldBounds = true;
     character.anchor.setTo(0.5, 0.5)
     game.camera.follow(character)
+
+    enemy = game.add.sprite(500, 100, 'enemy1')
+    game.physics.arcade.enable(enemy)
+    enemy.body.gravity.y = 200
+    enemy.body.collideWorldBounds = true;
+    enemy.anchor.setTo(0.5, 0.5)
 }
 
 function update() {
 
   if (game.input.activePointer.isDown)
       {
-
           fire();
       }
+
     game.physics.arcade.collide(character, floor)
+    game.physics.arcade.collide(enemy, floor)
+    game.physics.arcade.collide(enemy, character)
+    if (game.physics.arcade.collide(arrow, enemy)){
+      explode(enemy);
+      destroySprite(enemy)
+
+    }
 
   if (game.input.keyboard.isDown(Phaser.Keyboard.A))
     {
@@ -86,7 +105,6 @@ function update() {
         character.y += speed;
         character.angle = -75;
     }
-
   if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
     {
         character.body.velocity.setTo(0, -400);
@@ -107,16 +125,17 @@ function fire() {
         var shot = arrow.getFirstDead();
         shot.anchor.setTo(0.9,0.5)
         shot.rotation = game.physics.arcade.angleToPointer(character)
-        console.log(shot.rotation)
-        //arrow.rotation = game.physics.arcade.angleToPointer(arrow) - 90;
         shot.reset(character.x, character.y);
         game.physics.arcade.moveToPointer(shot, 300);
     }
 
 }
-function toRadians (angle){
-  return angle * (Math.PI / 180);
+
+function explode(enemy){
+  console.log("BOOM")
+
+  //enemy.events.onInputDown.add(destroySprite, this);
 }
-function toDegree (angle){
-  return angle * (180 / Math.PI);
+function destroySprite (sprite){
+  sprite.destroy()
 }
